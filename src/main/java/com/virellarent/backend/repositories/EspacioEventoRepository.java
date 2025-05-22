19dccb3e2bfffdb2b54bc69885e3dd14a9d509cd
Antony Cotrina <anthony77CG@hotmail.com>
2025-05-22 15:48:51 -0500

    Implementacion de la entidad y logica de gestion de espacios de eventos

---
+++ src/main/java/com/virellarent/backend/repositories/EspacioEventoRepository.java
@@ -0,0 +1,9 @@
+package com.virellarent.backend.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.virellarent.backend.entities.EspacioEvento;
+
+public interface EspacioEventoRepository extends JpaRepository<EspacioEvento, Long>{
+
+}
