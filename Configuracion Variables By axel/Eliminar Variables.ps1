866ff3fb7304dc1662f23458e9328ed6c4a2a887
AxelSantander26 <asantanderalcarraz@gmail.com>
2025-07-19 00:05:33 -0500

    conf Variables Backend

--- Configuracion Variables By axel/Eliminar Variables.ps1
+++
@@ -1,8 +0,0 @@
-Write-Host "Eliminando variables de entorno del SISTEMA..."
-
-[System.Environment]::SetEnvironmentVariable("DB_URL", $null, "Machine")
-[System.Environment]::SetEnvironmentVariable("DB_USER", $null, "Machine")
-[System.Environment]::SetEnvironmentVariable("DB_PASS", $null, "Machine")
-[System.Environment]::SetEnvironmentVariable("JWT_SECRET", $null, "Machine")
-
-Write-Host "Variables del sistema eliminadas."
